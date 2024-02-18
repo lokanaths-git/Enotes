fb99b21af55af034d9e7108c603075f807234f66
lokanaths-git <lokanath199866@gmail.com>
2024-02-18 17:28:12 +0530

    committed

--- Enotes/src/main/java/com/enote/User/UserDetails.java
+++ Enotes/src/main/java/com/enote/User/UserDetails.java
@@ -2,6 +2,7 @@ package com.enote.User;
 
 public class UserDetails {
 
+	private int id;
 	private String name;
 	private String email;
 	private String password;
@@ -17,6 +18,15 @@ public class UserDetails {
 		this.email = email;
 		this.password = password;
 	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 
 	public String getName() {
 		return name;
