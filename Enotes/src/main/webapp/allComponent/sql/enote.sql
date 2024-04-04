7b28c9450e21e123451f8b9f6b369597d5e644f7
lokanaths-git <lokanath199866@gmail.com>
2024-04-04 22:12:31 +0530

    committed

--- Enotes/src/main/webapp/allComponent/sql/enote.sql
+++ Enotes/src/main/webapp/allComponent/sql/enote.sql
@@ -5,3 +5,16 @@ CREATE TABLE `enote`.`user` (
   `password` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`id`) 
 );
+
+
+
+CREATE TABLE `enote`.`addnote` (  
+  `id` INT(10) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(50),
+  `content` VARCHAR(100),
+  `date` TIMESTAMP DEFAULT NOW(),
+  `uid` INT(10) NOT NULL,
+  PRIMARY KEY (`id`) ,
+  FOREIGN KEY (`uid`) REFERENCES `enote`.`user`(`id`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=INNODB CHARSET=utf8 COLLATE=utf8_general_ci;
+
